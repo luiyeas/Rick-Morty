74407a567dde9182fa82125b7ae920ec6a916cd3
Luis Miguel Navarro <luiyeas@gmail.com>
2025-01-29 18:20:30 +0100

    Basic character list screen & viewmodel

---
+++ app/src/main/java/com/lnavarro/rickmorty/ui/screens/characterlist/CharacterList.kt
@@ -0,0 +1,9 @@
+package com.lnavarro.rickmorty.ui.screens.characterlist
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CharacterListScreen(modifier : Modifier, characterListViewModel: CharacterListViewModel) {
+
+}
---
+++ app/src/main/java/com/lnavarro/rickmorty/ui/screens/characterlist/CharacterListViewModel.kt
@@ -0,0 +1,10 @@
+package com.lnavarro.rickmorty.ui.screens.characterlist
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CharacterListViewModel @Inject constructor() : ViewModel() {
+
+}
