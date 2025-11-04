7eb11451040a44e619824db0cbad6b96e74552a7
lnavarrofrutos <luis.navarro@worldwidemobility.io>
2025-11-04 16:44:09 +0100

    fix: URL encode/decode characterJson for navigation

--- app/src/main/java/com/lnavarro/rickmorty/ui/navigation/Navigation.kt
+++ app/src/main/java/com/lnavarro/rickmorty/ui/navigation/Navigation.kt
@@ -7,11 +7,14 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.lnavarro.rickmorty.ui.model.CharacterUI
 import com.lnavarro.rickmorty.ui.screens.characterdetail.CharacterDetailScreen
 import com.lnavarro.rickmorty.ui.screens.characterdetail.CharacterDetailViewModel
 import com.lnavarro.rickmorty.ui.screens.characterlist.CharacterListScreen
 import com.lnavarro.rickmorty.ui.screens.characterlist.CharacterListViewModel
 import kotlinx.serialization.json.Json
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 
 sealed class Screen(val route: String) {
     data object CharacterList : Screen("characterList")
@@ -31,7 +34,8 @@ fun RickAndMortyNavHost(
             CharacterListScreen(characterListViewModel = listViewModel,
                 onCharacterClick = { characterUI ->
                     val characterJson = Json.encodeToString(CharacterUI.serializer(), characterUI)
-                    navController.navigate("${Screen.CharacterDetail.route}/$characterJson")
+                    val encodedCharacterJson = URLEncoder.encode(characterJson, StandardCharsets.UTF_8.toString())
+                    navController.navigate("${Screen.CharacterDetail.route}/$encodedCharacterJson")
                 })
         }
         composable(
